ed23172d3916525f7b2dd0653c5962a9ef625bc2
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-04 15:22:54 +0530

    Identity Controller register route

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -51,7 +51,53 @@ const resgiterUser = async (req, res) => {
 
 
 // user login
+const loginUser = async (req, res) => {
+  logger.info("Login endpoint hit...");
+  try {
+    const { error } = validatelogin(req.body);
+    if (error) {
+      logger.warn("Validation error", error.details[0].message);
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      logger.warn("Invalid user");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // user valid password or not
+    const isValidPassword = await user.comparePassword(password);
+    if (!isValidPassword) {
+      logger.warn("Invalid password");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+
+    res.json({
+      accessToken,
+      refreshToken,
+      userId: user._id,
+    });
+  } catch (e) {
+    logger.error("Login error occured", e);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
 // refresh token 
 
---
+++ identity-service/src/routes/identity-service.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {
+  resgiterUser,
+} = require("../controllers/identity-controller");
+
+const router = express.Router();
+
+router.post("/register", resgiterUser);
+
+
+module.exports = router;
