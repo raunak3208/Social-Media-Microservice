90ba392a1abd4811fec3107d12970c376113c738
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-06 22:53:40 +0530

    DDos +IPbased limitter

---
+++ post-service/src/server.js
@@ -0,0 +1,94 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Redis = require("ioredis");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const RedisStore = require("rate-limit-redis");
+
+const postRoutes = require("./routes/post-routes");
+const errorHandler = require("./middleware/errorHandler");
+const logger = require("./utils/logger");
+const { connectToRabbitMQ } = require("./utils/rabbitmq");
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => logger.info("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+//middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body: ${JSON.stringify(req.body)}`);
+  next();
+});
+
+
+// DDOS Protection and rate limiting
+
+const ddosLimiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 10, // 10 requests per second per IP
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`DDOS protection triggered for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests. Slow down.",
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+// apply globally
+app.use(ddosLimiter);
+
+
+//IP based Sensitive Endpoint Limiter 
+
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50, // max 50 requests per IP
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests",
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+
+// ================= Routes =================
+
+app.use(
+  "/api/posts",
+  sensitiveEndpointsLimiter,
+  (req, res, next) => {
+    req.redisClient = redisClient;
+    next();
+  },
+  postRoutes
+);
+
+app.use(errorHandler);
+
