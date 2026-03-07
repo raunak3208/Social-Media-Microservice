23779185420fc654a1ab805fc944e97c92a93214
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-07 15:02:49 +0530

    media service server setup

---
+++ media-service/src/server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const mediaRoutes = require("./routes/media-routes");
+const errorHandler = require("./middleware/errorHandler");
+const logger = require("./utils/logger");
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => logger.info("Connected to mongodb"))
+  .catch((e) => logger.error("Mongo connection error", e));
+  
+//middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+// if you want DDOS protection, you can add rate limiting here
+
+
+app.use("/api/media", mediaRoutes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Identity service running on port ${PORT}`);
+});
+
+//unhandled promise rejection
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled Rejection at", promise, "reason:", reason);
+});
