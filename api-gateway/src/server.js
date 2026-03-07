7d7cb286d55270532f5bda5a83a08254e12b6213
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-07 15:04:05 +0530

    proxy added for post service

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -96,14 +96,45 @@ app.use(
   })
 );
 
+//setting up proxy for our media service
+app.use(
+  "/v1/media",
+  validateToken,
+  proxy(process.env.MEDIA_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+      if (!srcReq.headers["content-type"].startsWith("multipart/form-data")) {
+        proxyReqOpts.headers["Content-Type"] = "application/json";
+      }
+
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from media service: ${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+    parseReqBody: false,
+  })
+);
+
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  logger.info(`API Gateway is running on port ${PORT}`);
+  logger.info(
+    `API Gateway is running on port ${PORT}`
+  );
   logger.info(
     `Identity service is running on port ${process.env.IDENTITY_SERVICE_URL}`
   );
-   logger.info(
+  logger.info(
     `Post service is running on port ${process.env.POST_SERVICE_URL}`
     );
+  logger.info(
+    `Media service is running on port ${process.env.MEDIA_SERVICE_URL}`
+  );
 });
